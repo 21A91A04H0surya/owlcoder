3823e43c3c720933fb2cb2fa03a53c2f2cb68c75
21A91A04H0surya <98006212+21A91A04H0surya@users.noreply.github.com>
2024-01-19 21:47:35 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Kadane's Algorithm/kadanes-algorithm.py
@@ -0,0 +1,48 @@
+#User function Template for python3
+
+class Solution:
+    ##Complete this function
+    #Function to find the sum of contiguous subarray with maximum sum.
+    def maxSubArraySum(self,arr,N):
+        ##Your code here
+        maxi=0
+        su=0
+        for i in arr:
+            su+=i
+            if su < 0:
+                su=0
+            if su > maxi:
+                maxi=su
+        if maxi == 0:
+            return max(arr)
+        else:
+            return maxi
+                
+            
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+ 
+def main():
+        T=int(input())
+        while(T>0):
+            
+            n=int(input())
+            
+            arr=[int(x) for x in input().strip().split()]
+            
+            ob=Solution()
+            
+            print(ob.maxSubArraySum(arr,n))
+            
+            T-=1
+
+
+if __name__ == "__main__":
+    main()
+# } Driver Code Ends
