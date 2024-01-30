c62c296413253c08590a5d06ccb18c1dbff3c126
21A91A04H0surya <98006212+21A91A04H0surya@users.noreply.github.com>
2024-01-31 00:43:30 +0530

    Time: 51 ms (77.47%) | Memory: 16.7 MB (57.55%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,11 +1,12 @@
 class Solution:
     def fun(self,ind,la,arr,o,ans,tar):
-        if ind>=la:return
-        if tar==0:
-            a=o.copy()
-            print(a)
-            ans.append(a)
-        if(arr[ind] > tar): return
+        if ind>=la:
+            if tar==0:
+                a=o.copy()
+                print(a)
+                ans.append(a)
+            return
+        # if(arr[ind] > tar): return
         '''if pick'''
         if(arr[ind] <= tar):
             o.append(arr[ind])
@@ -16,8 +17,6 @@ class Solution:
     def combinationSum(self, arr: List[int], tar: int) -> List[List[int]]:
         ans=[]
         o=[]
-        #we can do for the multiple cases
-        arr.sort()
         m=len(arr)
         self.fun(0,m,arr,o,ans,tar)
         return ans
