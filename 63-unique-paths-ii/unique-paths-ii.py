3930a50e0ec0f2d0897cebc2b1cc3e9b57d6768c
21A91A04H0surya <98006212+21A91A04H0surya@users.noreply.github.com>
2024-02-11 22:10:19 +0530

    Time: 46 ms (57.12%) | Memory: 16.8 MB (60.96%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.py
+++ 63-unique-paths-ii/unique-paths-ii.py
@@ -2,9 +2,11 @@ class Solution:
     def mia(self,i,j,m,n,l):
         if i>=m or j >= n or l[i][j]==1:
             return 0
-        
         if i == m-1 and j == n-1:
-            return 1        
+            return 1
+        
+        # if l[i][j]==1:
+        #     return 0
         if dp[i][j]!=-1:
             return dp[i][j]
         dp[i][j]=self.mia(i+1,j,m,n,l)+self.mia(i,j+1,m,n,l)
