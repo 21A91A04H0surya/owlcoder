271697ad2ad44d46b045534fde96e15f578556e9
21A91A04H0surya <98006212+21A91A04H0surya@users.noreply.github.com>
2024-02-11 22:08:00 +0530

    Time: 39 ms (89.08%) | Memory: 16.7 MB (60.96%) - LeetSync

---
+++ 63-unique-paths-ii/unique-paths-ii.py
@@ -0,0 +1,19 @@
+class Solution:
+    def mia(self,i,j,m,n,l):
+        if i>=m or j >= n or l[i][j]==1:
+            return 0
+        
+        if i == m-1 and j == n-1:
+            return 1        
+        if dp[i][j]!=-1:
+            return dp[i][j]
+        dp[i][j]=self.mia(i+1,j,m,n,l)+self.mia(i,j+1,m,n,l)
+        return dp[i][j]
+    def uniquePathsWithObstacles(self, l: List[List[int]]) -> int:
+        m=len(l)
+        n=len(l[0])
+        global dp
+        dp=[[-1]*101 for i in range(101)]
+        return self.mia(0,0,m,n,l)
+    
+        
