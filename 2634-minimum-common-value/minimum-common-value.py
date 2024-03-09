bf01356022765e4c00e48820c40c8e5cf5ebf532
21A91A04H0surya <98006212+21A91A04H0surya@users.noreply.github.com>
2024-03-09 17:26:11 +0530

    Time: 363 ms (29.62%) | Memory: 45.9 MB (5.87%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.py
+++ 2634-minimum-common-value/minimum-common-value.py
@@ -1,12 +1,12 @@
+from collections import Counter
 class Solution:
     def getCommon(self, l1: List[int], l2: List[int]) -> int:
-        while len(l1)> 0 and len(l2)>0:
-            if l1[0]==l2[0]:
-                return l1[0]
-            elif l1[0]>l2[0]:
-                l2.pop(0)
-            else:
-                l1.pop(0)
+        dic1=Counter(l1)
+        dic2=Counter(l2)
+        for i in dic1:
+            if i in dic2:
+                return i
         return -1
+
         
         
