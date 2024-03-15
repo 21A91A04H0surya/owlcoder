5d237ce88b220203da5d822b21dfd169e22ea160
21A91A04H0surya <98006212+21A91A04H0surya@users.noreply.github.com>
2024-03-15 22:51:42 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Linked List that is Sorted Alternatingly/linked-list-that-is-sorted-alternatingly.py
@@ -0,0 +1,76 @@
+#User function Template for python3
+
+
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+class Solution:
+    def sort(self, h1):
+        #return head
+        l=[]
+        temp=h1
+        while temp!=None:
+            l.append(temp.data)
+            temp=temp.next
+        l.sort()
+        
+        
+        
+        
+        i=0
+        tem=h1
+        while tem!=None:
+            tem.data=l[i]
+            i+=1
+            tem=tem.next
+        return h1
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+class Llist:
+    def __init__(self):
+        self.head=None
+    
+    def insert(self,data,tail):
+        node=Node(data)
+        
+        if not self.head:
+            self.head=node
+            return node
+        
+        tail.next=node
+        return node
+        
+
+def printList(head):
+    while head:
+        print(head.data,end=' ')
+        head=head.next
+        
+if __name__ == '__main__':
+    t=int(input())
+    
+    for tcs in range(t):
+        
+        n1=int(input())
+        arr1=[int(x) for x in input().split()]
+        ll1=Llist()
+        tail=None
+        for nodeData in arr1:
+            tail=ll1.insert(nodeData,tail)
+            
+        
+        ob = Solution()
+        resHead=ob.sort(ll1.head)
+        printList(resHead)
+        print()
+        
+    
+    
+# } Driver Code Ends
