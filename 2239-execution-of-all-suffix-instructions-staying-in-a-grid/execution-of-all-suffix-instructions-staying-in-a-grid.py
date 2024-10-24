9285b3d08a5ca38fa2e95d8de4aef49cd24fc2dd
21A91A04H0surya <98006212+21A91A04H0surya@users.noreply.github.com>
2024-10-24 11:02:00 +0530

    Time: 564 ms (80.60%) | Memory: 16.6 MB (59.91%) - LeetSync

--- 2239-execution-of-all-suffix-instructions-staying-in-a-grid/execution-of-all-suffix-instructions-staying-in-a-grid.py
+++ 2239-execution-of-all-suffix-instructions-staying-in-a-grid/execution-of-all-suffix-instructions-staying-in-a-grid.py
@@ -1,32 +1,24 @@
 from typing import List
-
 class Solution:
     def executeInstructions(self, n: int, startpos: List[int], s: str) -> List[int]:
-        result = []
-        
-        # Loop through each starting position in the string
+        result=[]
         for i in range(len(s)):
-            row, col = startpos  # Reset to the initial position for each starting index
-            cnt = 0
-            
-            # Process commands starting from index i
-            for j in range(i, len(s)):
-                if s[j] == 'R' and col + 1 < n:
-                    col += 1
-                    cnt += 1
-                elif s[j] == 'L' and col - 1 >= 0:
-                    col -= 1
-                    cnt += 1
-                elif s[j] == 'U' and row - 1 >= 0:
-                    row -= 1
-                    cnt += 1
-                elif s[j] == 'D' and row + 1 < n:
-                    row += 1
-                    cnt += 1
+            cnt=0
+            row,col=startpos[0],startpos[1]
+            for j in range(i,len(s)):
+                if s[j]=='R' and col+1<n:
+                    cnt+=1
+                    col+=1
+                elif s[j]=='L' and col-1>=0:
+                    col-=1
+                    cnt+=1
+                elif s[j]=='U' and row-1>=0:
+                    cnt+=1
+                    row-=1
+                elif s[j]=='D' and row+1<n:
+                    row+=1
+                    cnt+=1
                 else:
-                    break  # Stop if move is invalid
-            
-            # Append the count of valid moves for this starting index
+                    break
             result.append(cnt)
-        
         return result
